c50f316eba1c0e23bf554df13e077ca4778dfd6b
prasaa18 <prasadsnaik18@gmail.com>
2023-09-27 00:09:07 +0530

    first commit

---
+++ src/app/shared/global-constants.ts
@@ -0,0 +1,17 @@
+export class GlobalConstants{
+    //message
+
+    public static genericError:string="something went wrong,please try again later";
+
+    //regex
+    public static nameRegex:string="[a-zA-Z0-9 ]*";
+    
+    public static emailRegex:string="[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
+
+    public static contactNumberRegex:string="^[e0-9]{10,10}$";
+
+    public static error:string="error";
+
+
+
+}
