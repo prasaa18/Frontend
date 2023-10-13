ad4f2f08125c32862dba5ae014324e963cf3a2b8
prasaa18 <prasadsnaik18@gmail.com>
2023-10-13 22:48:01 +0530

    changepassword and logout

--- src/app/material-component/dialog/change-password/change-password.component.ts
+++ src/app/material-component/dialog/change-password/change-password.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { error } from 'console';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { SnackbarService } from 'src/app/services/snackbar.service';
 import { UserService } from 'src/app/services/user.service';
+import { GlobalConstants } from 'src/app/shared/global-constants';
 
 @Component({
   selector: 'app-change-password',
@@ -14,7 +16,7 @@ export class ChangePasswordComponent implements OnInit {
 
   oldPassword=true;
   newPassword=true;
-  confirmPassword=true
+  confirmpassword=true;
   changePasswordForm:any=FormGroup;
   responceMessage:any;
   
@@ -27,8 +29,64 @@ export class ChangePasswordComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.changePasswordForm=this.formBuilder.group,
+    this.changePasswordForm=this.formBuilder.group({
+      oldPassword:[null,Validators.required],
+      newPassword:[null,Validators.required],
+      confirmpassword:[null,Validators.required],
+    })
     
   }
+
+  toggleOldPasswordVisibility() {
+    this.oldPassword = !this.oldPassword;
+
+  }
+  toggleNewPasswordVisibility(){
+   this.newPassword=!this.newPassword;
+  }
+  toggleConfirmPasswordVisibility(){
+    this.confirmpassword=!this.confirmpassword;
+  }
+
+  validateSubmit()
+  {
+    
+    if(this.changePasswordForm.controls['newPassword'].value!=this.changePasswordForm.controls['confirmpassword'].value)
+    return true;
+  else{
+    return false;
+  }
+  }
+
+  handleSubmit(){
+    this.ngxService.start();
+    var formData=this.changePasswordForm.value;
+    var Data={
+      oldPassword:formData.oldPassword,
+      newPassword:formData.newPassword,
+      confirmpassword:formData.confirmpassword
+
+    }
+  this.userService.chngePassword(Data).subscribe((response:any)=>{
+    this.ngxService.stop();
+    this.responceMessage=response.message;
+    this.dialogRef.close();
+    this.snackBarService.openSnackbar(this.responceMessage,"Success")
+  },(error)=>{
+          console.log(error);
+          this.ngxService.stop();
+          if(error.error?.message){
+            this.responceMessage=error.error?.message;
+          }
+          else{
+            this.responceMessage=GlobalConstants.genericError;
+
+          }
+          this.snackBarService.openSnackbar(this.responceMessage,GlobalConstants.error);
+  })
+  
+
+}
+
 
 }
